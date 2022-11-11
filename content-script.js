1ea19dee344c286b964ef0267d4b0ff21dc73de3
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-11-11 21:47:31 +0530

    Update content-script.js added functions for blurring chats and messages

--- content-script.js
+++ content-script.js
@@ -1,57 +1,43 @@
-// const messageIdentifier = "div._22Msk";
-// const chatIdentifier = "div.lhggkp7q.ln8gz9je.rx9719la";
-// // document.addEventListener("load", onLoadCallback);
+//By default the content scripts run at After Dom load done. checkout https://developer.chrome.com/docs/extensions/mv3/content_scripts/#capabilities
 
-// // function onLoadCallback() {
-// //   const messages = document.querySelectorAll(chatIdentifier);
-// //   console.log(messages);
-// //   messages.forEach((mes) => addFilter(mes));
-// // }
+//attaching style tag to head
+(() => {
+  const style = document.createElement("style");
+  style.textContent = `.blurred{
+         filter: blur(4px);
+       }
+       .blurred:hover{
+         filter: blur(0px);
+       }`;
+  document.head.appendChild(style);
+})();
 
-// // function addFilter(element) {
-// //   element.style.filter = "blur(20px)";
-// // }
+const MESSAGE_IDENTIFIER = ".message-out > * , .message-in > *";
+const CHAT_IDENTIFIER = "div.lhggkp7q.ln8gz9je.rx9719la ";
 
-// function setStyleTagToDom() {
-//   const style = document.createElement("style");
-//   style.textContent = `.blurred{
-//         filter: blur(4px);
-//       }
-//       .blurred:hover{
-//         filter: blur(0px);
-//       }`;
-//   document.head.appendChild(style);
-//   console.log(style);
-// }
+const app = document.querySelector("#app");
 
-// function onPageLoadCallback() {
-//   setStyleTagToDom();
-// }
+let isChatLoaded = false;
 
-// //left side chats
-// function onChatsLoaded() {
-//   const messages = document.querySelectorAll(chatIdentifier);
-//   messages.forEach((message) => {
-//     message.classList.add("blurred");
-//     message.addEventListener("click", (e) => {
-//       console.log(e);
-//       blurRightSide();
-//     });
-//   });
-// }
+function blurChats() {
+  console.log("Blurring the left chats");
+  const chats = document.querySelectorAll(CHAT_IDENTIFIER);
+  chats.forEach((chat) => chat.classList.add("blurred"));
+}
 
-// function blurRightSide() {
-//   const messages = document.querySelectorAll(messageIdentifier);
-//   messages.forEach((message) => {
-//     console.log(messages);
-//     message.classList.add("blurred");
-//   });
-// }
-// setTimeout(() => {
-//   console.log("right side blurring");
-//   onChatsLoaded();
-// }, 10000);
-
-// onPageLoadCallback();
-
-//by default the content scripts run at After Dom load done. checkout https://developer.chrome.com/docs/extensions/mv3/content_scripts/#capabilities
+function blurMessages() {
+  console.log("Blurring the right messages");
+  const messages = document.querySelectorAll(MESSAGE_IDENTIFIER);
+  console.log(messages);
+  messages.forEach((message) => message.classList.add("blurred"));
+}
+function callback() {
+  if (!isChatLoaded) {
+    blurChats();
+    isChatLoaded = true;
+  }
+  blurMessages();
+}
+const config = { attributes: false, childList: true, subtree: true };
+const mutationObserver = new MutationObserver(callback);
+mutationObserver.observe(app, config);

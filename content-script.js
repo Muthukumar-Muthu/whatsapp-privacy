d83505461fd6d3574d9bd0671882370fa0e00cff
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-11-15 20:18:32 +0530

    modified content-script

--- content-script.js
+++ content-script.js
@@ -1,12 +1,13 @@
 //By default the content scripts run at After Dom load done. checkout https://developer.chrome.com/docs/extensions/mv3/content_scripts/#capabilities
 
 //attaching style tag to head
+// const CLASS_NAME = "blur-wp"
 (() => {
   const style = document.createElement("style");
-  style.textContent = `.blurred{
+  style.textContent = `.${CLASS_NAME}{
          filter: blur(4px);
        }
-       .blurred:hover{
+       .${CLASS_NAME}:hover{
          filter: blur(0px);
        }`;
   document.head.appendChild(style);
@@ -17,72 +18,126 @@ const identifers = {
   chats: "div.lhggkp7q.ln8gz9je.rx9719la",
 };
 
-const app = document.querySelector("#app");
+/*
+ const app = document.querySelector("#app");
+ let isChatLoaded = false;
+ const config = { attributes: false, childList: true, subtree: true };
+ const mutationObserver = new MutationObserver(c);
+ mutationObserver.observe(app, config);
 
-let isChatLoaded = false;
+stucture of blurlist from event (from popup.js event)
+ [
+   {
+     identifier: "d",
+     type: "name",
+     id: "name",
+   },
+ ];
+*/
 
-function blurChats() {
-  console.log("Blurring the left chats");
-  const chats = document.querySelectorAll(identifers.chats);
-  chats.forEach((chat) => chat.classList.add("blurred"));
+//global declarations
+
+const CLASS_NAME = "blur-wp";
+const BLUR_LIST = "blur_list";
+
+const extensionStorage = {
+  set: ({ key, value }) => {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.set({ [key]: JSON.stringify(value) }, function () {
+          console.log("Value is set to " + value);
+        });
+        resolve({ key, value });
+      } catch (error) {
+        console.error(error);
+        reject(error);
+      }
+    });
+  },
+  get: (key) => {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.get([key], function (result) {
+          console.log("Value currently is " + result.key);
+          resolve(JSON.parse(result[key]));
+        });
+      } catch (error) {
+        console.error(error);
+        reject(error);
+      }
+    });
+  },
+};
+
+function blur(list = []) {
+  const identifiers = list.map((value) => value.identifer);
+  identifiers.forEach((identifer) => {
+    document
+      .querySelectorAll(identifer)
+      .forEach((node) => node.classList.add(CLASS_NAME));
+  });
 }
 
-function blurMessages() {
-  console.log("Blurring the right messages");
-  const messages = document.querySelectorAll(identifers.messages);
-  console.log(messages);
-  messages.forEach((message) => message.classList.add("blurred"));
+function unBlur(list = []) {
+  const identifiers = list.map((value) => value.identifer);
+  identifiers.forEach((identifer) => {
+    document
+      .querySelectorAll(identifer)
+      .forEach((node) => node.classList.remove(CLASS_NAME));
+  });
 }
-function startBlur() {
-  if (!isChatLoaded) {
-    blurChats();
-    isChatLoaded = true;
-  }
-  blurMessages();
+
+function diffInBlurList(previous = [], present = []) {
+  const unBlurList = previous.filter(
+    (previousValue) =>
+      !present.find((presentValue) => presentValue.type === previousValue.type)
+  );
+  const blurList = present.filter((presentValue) =>
+    previous.find((previousValue) => previousValue.type === presentValue.type)
+  );
+  return {
+    unBlurList,
+    blurList,
+  };
 }
+
+//after loading the data (after the loader finished)
+
+const style = document.createElement("style");
+style.textContent = `.${CLASS_NAME}{
+         filter: blur(4px);
+       }
+       .${CLASS_NAME}:hover{
+         filter: blur(0px);
+       }`;
+document.head.appendChild(style);
+
+extensionStorage
+  .get(BLUR_LIST)
+  .then((blurList) => {
+    const identifiers = blurList.map((value) => value.identifer);
+    blur(identifiers);
+  })
+  .catch(console.error);
+
+//if popup.js sends any event
 
 chrome.runtime.onMessage.addListener(function (request) {
   if (request?.blur) {
     console.log("Blurring", request.blurList);
     if (request?.blurList) {
-      const { blurList } = request;
-      console.log(blurList, "before setting localstorage");
-      extensionStorage.set({ key: "blur", value: JSON.stringify(blurList) });
-      blurThings(blurList);
+      const blurListFromEvent = request.blurList;
+      extensionStorage
+        .get(BLUR_LIST)
+        .then((currentList) => {
+          const { blurList, unBlurList } = diffInBlurList(
+            currentList,
+            blurListFromEvent
+          );
+          blur(blurList);
+          unBlur(unBlurList);
+        })
+        .catch(console.error);
     } else console.error("Blur list absent");
   }
 });
-
-function blurThings(blurList) {
-  console.log(blurList);
-  const blurIdentifiers = blurList
-    .map((blur) => (identifers[blur] !== "undefined" ? identifers[blur] : null))
-    .filter((value) => value !== null);
-  console.log(blurIdentifiers);
-  blurIdentifiers.forEach((identifier) => {
-    const nodes = document.querySelectorAll(identifier);
-    nodes.forEach((node) => {
-      node.classList.add("blurred");
-    });
-  });
-}
-
-const extensionStorage = {
-  set: ({ key, value }) => {
-    console.log(key, value, { [key]: value });
-    chrome.storage.local.set({ [key]: value }, function () {
-      console.log("Value is set to " + value);
-    });
-  },
-  get: (key) => {
-    chrome.storage.local.get([key], function (result) {
-      console.log("Value currently is " + result.key);
-    });
-  },
-};
-
-const config = { attributes: false, childList: true, subtree: true };
-const mutationObserver = new MutationObserver(c);
-mutationObserver.observe(app, config);
-
-//modeify code and at start state and pop state sync
